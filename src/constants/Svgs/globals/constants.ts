6261dfda98bb84e1c46b46e7f66613be6c0cf389
Saim Shahzad <saimshehzad2030@gmail.com>
2025-01-28 16:50:43 +0500

    first commit

--- src/constants/Svgs/globals/constants.ts
+++ src/constants/Svgs/globals/constants.ts
@@ -1,7 +1,7 @@
-export const CAST = [{ name: "taha alhamid", image: '/assets/Fadi.png' },
-{ name: "ali taifour", image: '/assets/Jabal.png' },
-{ name: "fadi Michael", image: '/assets/Omar.png' },
+export const CAST = [{ name: "taha alhamid", image: '/assets/Amir.png' },
+{ name: "ali taifour", image: '/assets/Omar.png' },
+{ name: "fadi Michael", image: '/assets/Fadi.png' },
 { name: "sara alzoul", image: '/assets/Sara.png' },
 { name: "Basil h hassan", image: '/assets/Yousef.png' },
-{ name: "mohamed Baoqbah", image: '/assets/Amir.png' }
+{ name: "mohamed Baoqbah", image: '/assets/Jabal.png' }
 ]
